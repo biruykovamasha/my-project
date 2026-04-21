5067b36655e479a2d9bcd3bef3557d33888697d8
biruykovamasha <biruykovamasha87@gmail.com>
2026-04-21 16:55:09 +0300

    Добавлена 7 лабораторная

--- lab7.py
+++ lab7.py
@@ -65,7 +65,7 @@ print("Длина кортежа (количество человек в ком�
 team_sorted = tuple(sorted(team))
 print("Отсортированный кортеж по алфавиту:", team_sorted)
 
-surname = "Бирюкова"
+surname = "Дворянинова"
 count = team.count(surname)
 
 if count > 0:
